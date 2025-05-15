0adad95ac459afda42676720485948c7a11c7100
Gareth Chng <goofygoobers20230313@gmail.com>
2025-05-15 16:04:15 +0800

    IDK

--- background/test.py
+++ background/test.py
@@ -1,27 +1,23 @@
-from failsafe import failsafe
 from djitellopy import tello
+from failsafe import failsafe
 
-    
 drone = tello.Tello()
+
 
 @failsafe(drone)
 def main():
-    drone.connect()
-    drone.takeoff()
+    drone.connect()# Connects to the drone
+    drone.takeoff()# Tells the drone to Take off
     drone.set_video_direction(drone.CAMERA_FORWARD)
     drone.set_video_fps(drone.FPS_30)
     drone.stream_on()
+    drone.move_up(20)# Tells the drone to Fly up
     while True:
         drone.rotate_clockwise(180)
-
+    drone.land()
 
 
 
 if __name__ == "__main__":
     main()
 
-
-# Run the code and wait for the drone to start rotating
-# Press key 'q' and see if the drone stops and land
-
-# Run the code using: python test.py
